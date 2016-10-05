1c2797ea01d96853051ec6e5f67adaaf8e48f7b0
Kaustabh04 <kaustabh04@gmail.com>
2016-10-05 23:59:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+  //Variables storing Name,Age,Date of Birth & Place of Birth
+  
+  var name = "John"
+  var age = 25;
+  var dateofbirth = "04/04/1977";
+  var placeofbirth = "London";
+  
+  console.log(name);
+  console.log(age);
+  console.log(dateofbirth);
+  console.log(placeofbirth);
